bafa84f92529c0de1263f59613cb2619a55a4a49
BugHunter-X <askzenai@gmail.com>
2024-12-29 07:22:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,9 @@
+```javascript
+// pages/index.js
+export default function Home() {
+  return (
+    <div>Hello World</div>
+  );
+}
+```
+This simple Next.js 15 app produces a unexpected error when deployed to Vercel. The error message is cryptic and unhelpful, making it difficult to identify the cause.
